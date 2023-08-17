c1ec939ae1697d335c482253fc25e9c2c6d690c8
Евгений Матрос <mr@MacBook-Air-Evgenij.local>
2023-08-17 16:18:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {MainScreen, CreateHabbit, Statistics, Settings}  from './components';
+import Meditations from "./imgs/meditation.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="conteiner">
+        <img alt="meditation" src={Meditations} className='imgMeditation'/>
+        <header className='glass'>
+          <div className='logo'>😎 Habbits Tracker</div>
+            <nav>
+            <ul>
+              <li>Main info</li>
+              <li>Statistics</li>
+              <li>Settings</li>
+              <li>Add Habbit</li>
+            </ul>
+          </nav>
+        </header>
+         <section className='glass'>
+          <MainScreen/>
+          <CreateHabbit />
+          <Statistics/>
+          <Settings/>
+         </section>
+        <div className="circleGradient"></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import MainScreen from "./main_screen/MainScreen";
+import CreateHabbit from "./create_habbit/CreateHabbit";
+import Statistics from "./statistics/Statistics";
+import Settings from "./settings/Settings";
+
+export {MainScreen, CreateHabbit, Statistics, Settings};
